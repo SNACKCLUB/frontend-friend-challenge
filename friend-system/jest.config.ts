6484a15abaaba8055139e5eb63ee123b8668c60b
Carlos Lima <carlosedu151@gmail.com>
2025-02-09 19:26:56 -0300

    chore: add config to execute unit tests

--- friend-system/jest.config.ts
+++ friend-system/jest.config.ts
@@ -1,12 +1,18 @@
-module.exports = {
+import type { Config } from "jest";
+
+const config: Config = {
+  preset: "ts-jest",
   testEnvironment: "jsdom",
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
   moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
   testMatch: ["<rootDir>/src/components/**/__tests__/**/*.(spec|test).{ts,tsx}"],
-  transform: {
-    "^.+\\.(ts|tsx)$": "babel-jest",
-  },
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1",
   },
+  transform: {
+    "^.+\\.(ts|tsx|js|jsx)$": "babel-jest",
+  },
+  transformIgnorePatterns: ["<rootDir>/node_modules/"],
 };
+
+export default config;
